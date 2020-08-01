f4f4f0cb9fec6bf17878f1fddec935bc3acd5dc8
Ergo <termigiova@me.com>
2020-08-01 14:54:12 -0500

    Add sign up component

--- src/components/pages/sign-in-and-sign-up/sign-in-and-sign-up.component.jsx
+++ src/components/pages/sign-in-and-sign-up/sign-in-and-sign-up.component.jsx
@@ -3,10 +3,12 @@ import React from 'react';
 import SignIn from '../../sign-in/sign-in.component';
 
 import './sign-in-and-sign-up.styles.scss';
+import SignUp from '../../sign-up/sign-up.component';
 
 const SignInAndSignUp = () => (
 	<div className='sign-in-and-sign-up'>
 		<SignIn />
+		<SignUp />
 	</div>
 );
 
