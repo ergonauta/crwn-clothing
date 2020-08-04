11d4f94b0a15b074955e5f3204fec71b6aebb2f5
Ergo <termigiova@me.com>
2020-08-03 19:08:18 -0500

    Update state to accept new SHOP_DATA

--- src/components/pages/shop/shop.component.jsx
+++ src/components/pages/shop/shop.component.jsx
@@ -8,11 +8,14 @@ class ShopPage extends React.Component {
 	constructor() {
 		super();
 
-		this.state = SHOP_DATA;
+		this.state = {
+			collections: SHOP_DATA
+		};
 	}
 
 	render() {
 		const { collections } = this.state;
+
 		return (<div className='shop-page'>
 			{
 				collections.map(({ id, ...otherCollectionProps }) => (
