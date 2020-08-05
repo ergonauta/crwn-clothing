514fbb0c12deb5a08642c5332d3717cc63e1d3a6
Ergo <termigiova@me.com>
2020-08-05 14:47:12 -0500

    Add selectors

--- src/redux/cart/cart.selectors.js
+++ src/redux/cart/cart.selectors.js
@@ -7,10 +7,23 @@ export const selectCartItems = createSelector(
 	cart => cart.cartItems
 );
 
+export const selectCartHidden = createSelector(
+	[selectCart],
+	cart => cart.hidden
+);
+
 export const selectCartItemsQuantity = createSelector(
 	[selectCartItems],
 	cartItems => cartItems.reduce(
 		(accumulatedQuantity, cartItem) => accumulatedQuantity + cartItem.quantity
+		, 0
+	)
+);
+
+export const selectCartTotal = createSelector(
+	[selectCartItems],
+	cartItems => cartItems.reduce(
+		(accumulatedTotal, cartItem) => (cartItem.quantity * cartItem.price) + accumulatedTotal
 		, 0
 	)
 );
