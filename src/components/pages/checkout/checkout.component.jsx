f995c18a3c2c1f4530251b4a01232b5c704f7fed
Ergo <termigiova@me.com>
2020-08-05 19:22:10 -0500

    Update notation

--- src/components/pages/checkout/checkout.component.jsx
+++ src/components/pages/checkout/checkout.component.jsx
@@ -28,7 +28,7 @@ const CheckoutPage = ({ cartItems, cartTotal }) => (
 		</div>
 		{
 			cartItems.map(cartItem => (
-				<CheckoutItem key={cartItem.id} cartItem={cartItem}></CheckoutItem>
+				<CheckoutItem key={cartItem.id} cartItem={cartItem} />
 			))
 		}
 		<div className='total'>
