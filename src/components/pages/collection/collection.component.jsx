2844afdcf8e573736c17b8e4c9b5dec1cb1bc10d
Ergo <termigiova@me.com>
2020-08-06 18:51:34 -0500

    Create Collection Component

---
+++ src/components/pages/collection/collection.component.jsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import { connect } from 'react-redux';
+import { createStructuredSelector } from 'reselect';
+
+import { selectCollection } from '../../../redux/shop/shop.selector';
+
+import CollectionItem from '../../collection-item/collection-item.collection';
+
+import './collection.styles.scss';
+
+const CollectionPage = ({ collection }) => {
+	const { title, items } = collection
+
+	return (
+		<div className='collection-page'>
+			<h2 className='title'>{title}</h2>
+			<div className='items'>
+				{
+					items.map(item => (
+						<CollectionItem key={item.id} item={item} />
+					))
+				}
+			</div>
+		</div>
+	)
+};
+
+const mapStateToProps = (state, ownProps) => createStructuredSelector({
+	collection: selectCollection(ownProps.match.params.collectionId)
+})
+
+export default connect(mapStateToProps)(CollectionPage);
